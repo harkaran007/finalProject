131e7ed4ed6ec3e21c56e7a79f488c7bb7dd341f
Mannoti78 <55302101+Mannoti78@users.noreply.github.com>
2020-12-19 16:59:24 -0500

    Mannoti

---
+++ Favmovies.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Movies
+{
+    public class Favmovies : RealmObject
+    {
+        public string name { get; set; }
+        public string username { get; set; }
+    }
+}
