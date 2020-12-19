442ef67e7631cda613584b02f9f7fe778283ae30
Mannoti78 <55302101+Mannoti78@users.noreply.github.com>
2020-12-19 16:57:20 -0500

    Mannoti

---
+++ Movies.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Movies
+{
+    public class Movies : RealmObject
+    {
+        public string name { get; set; }
+    }
+}
